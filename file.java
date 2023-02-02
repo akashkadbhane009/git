6e457a2ceff0c436248ce2784ff1f4fcc833c119
Akash K <akashkadbhane@gmail.com>
2023-02-02 15:19:34 +0530

    Third commit armstrong

--- file.java
+++ file.java
@@ -1,6 +1,6 @@
 Class fib{
 
     public static void main(String[] args){
-        System.out.println("prime numers till 20 numbers code");
+        System.out.println("armstrong number code");
     }
 }
