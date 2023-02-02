40a4ef0a694f7f9da801d63b2468bf37d30c6c6b
Akash K <akashkadbhane@gmail.com>
2023-02-02 15:07:42 +0530

    first commit fib

--- file.java
+++ file.java
@@ -1,1 +1,6 @@
-fibonacci series with 20 numbers and call commit.
+Class fib{
+
+    public static void main(String[] args){
+        System.out.println("fibonacci series with 20 numbers");
+    }
+}
